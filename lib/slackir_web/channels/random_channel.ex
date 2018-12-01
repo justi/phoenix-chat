c30852be06b1c3353fd4143a3fbb2c1eed8a961a
Justyna Wojtczak <justine84@gmail.com>
2018-12-01 10:52:50 +0100

    Save message to db

--- lib/slackir_web/channels/random_channel.ex
+++ lib/slackir_web/channels/random_channel.ex
@@ -18,8 +18,10 @@ defmodule Slackir.RandomChannel do
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (random:lobby).
+
   def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+    spawn(Slackir.Conversations, :create_message, [payload])
+    broadcast! socket, "shout", payload
     {:noreply, socket}
   end
 
